9cd4a3a2354d4b3ab3e56d18b4f2118afbf5af5f
Olga Filipova <chudaol@gmail.com>
2015-06-03 22:13:33 +0200

    added new lines in the end of scripts.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = {
                   "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n"       +
                   "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n"           +
                   "'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n"    +
-                  "})(window,document,'script','dataLayer','" + config.token + "');\b"                +
+                  "})(window,document,'script','dataLayer','" + config.token + "');\n"                +
                 "</script>";
       }
     }
