86601199285ec3e0ddc8ccfe133b3119b95eeb2c
Olga Filipova <chudaol@gmail.com>
2015-06-03 22:12:20 +0200

    added new lines in the end of scripts.

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ module.exports = {
           throw "Need to have option 'token' for Google Tag Manager plugin";
         }
 
-        return 	"<noscript>"                                                                        +
-                  "<iframe src=\"//www.googletagmanager.com/ns.html?id="                            +
-                  config.token                                                                      +
-                  "\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>"  +
-                "</noscript>"                                                                       +
-                "<script>"                                                                          +
-                  "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':"                      +
-                  "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],"       +
-                  "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src="           +
-                  "'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);"    +
-                  "})(window,document,'script','dataLayer','" + config.token + "');"                +
+        return 	"<noscript>\n"                                                                        +
+                  "<iframe src=\"//www.googletagmanager.com/ns.html?id="                              +
+                  config.token                                                                        +
+                  "\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>\n"  +
+                "</noscript>\n"                                                                       +
+                "<script>"                                                                            +
+                  "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n"                      +
+                  "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n"       +
+                  "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n"           +
+                  "'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n"    +
+                  "})(window,document,'script','dataLayer','" + config.token + "');\b"                +
                 "</script>";
       }
     }
