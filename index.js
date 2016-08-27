bd5fa55c6afbf26d09331ca3f26bc34668cb5b5b
Luis Herranz <luisherranz@gmail.com>
2016-08-27 18:36:20 +0200

    Initialise dataLayer if it hasn't been initialised before.

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ module.exports = {
 	hooks: {
 		page: function(page) {
 		  if (this.options.pluginsConfig.gtm.virtualPageViews) {
+		    dataLayer = dataLayer || [];
 		    dataLayer.push({
-          'event':'virtualPageView',
-          'page':{
-            'title': page.title,
-            'path': page.path,
-          }
-        });
+		          'event':'virtualPageView',
+		          'page':{
+		            'title': page.title,
+		            'path': page.path,
+		          }
+		        });
 	    }
 	    return page;
 	}
